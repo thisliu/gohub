48555e5d8eb7a64185b069f0de10b01ea740f3cd
thisliu <liuhao25@foxmail.com>
2022-08-21 10:02:34 +0800

    初始化日志库

--- main.go
+++ main.go
@@ -18,9 +18,14 @@ func init() {
 func main() {
 	// 配置初始化，依赖命令行 --env 参数
 	var env string
+
 	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
 	flag.Parse()
+
 	config.InitConfig(env)
+
+	// 初始化 Logger
+	bootstrap.SetupLogger()
 
 	// new 一个 Gin Engine 实例
 	router := gin.New()
