1582d3a2453f11116cabea22abdcbfc751187aa8
thisliu <liuhao25@foxmail.com>
2022-08-21 15:36:47 +0800

    Auth 中间件

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -33,3 +33,9 @@ func GetByMulti(loginID string) (userModel User) {
 		First(&userModel)
 	return
 }
+
+// Get 通过 ID 获取用户
+func Get(ID string) (userModel User) {
+	database.DB.Where("id = ?", ID).First(&userModel)
+	return
+}
