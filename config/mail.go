925d549e90ea3f387c3fa70de28f745647b642cd
thisliu <liuhao25@foxmail.com>
2022-08-21 13:23:08 +0800

    发送邮件验证码

--- config/mail.go
+++ config/mail.go
@@ -6,7 +6,6 @@ import "gohub/pkg/config"
 func init() {
 	config.Add("mail", func() map[string]interface{} {
 		return map[string]interface{}{
-			// 默认是 Mailhog 的配置
 			"smtp": map[string]interface{}{
 				"host":     config.Env("MAIL_HOST", "localhost"),
 				"port":     config.Env("MAIL_PORT", 1025),
